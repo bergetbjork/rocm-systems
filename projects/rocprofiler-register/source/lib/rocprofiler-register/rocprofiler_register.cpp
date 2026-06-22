290074a5ec0d12d2879c0142ef242c335aefd7b8
Venkateshwar Reddy Kandula <venkateshwarreddy.kandula@amd.com>
2026-06-22 17:14:38 +0000

    move hipfile in enum to last.

--- projects/rocprofiler-register/source/lib/rocprofiler-register/rocprofiler_register.cpp
+++ projects/rocprofiler-register/source/lib/rocprofiler-register/rocprofiler_register.cpp
@@ -169,8 +169,8 @@ enum rocp_reg_supported_library  // NOLINT(performance-enum-size)
     ROCP_REG_RCCL,
     ROCP_REG_ROCDECODE,
     ROCP_REG_ROCJPEG,
-    ROCP_REG_HIPFILE,
     ROCP_REG_ROCATTACH,
+    ROCP_REG_HIPFILE,
     ROCP_REG_LAST,
 };
 
@@ -240,15 +240,15 @@ ROCP_REG_DEFINE_LIBRARY_TRAITS(ROCP_REG_ROCJPEG,
                                "rocprofiler_register_import_rocjpeg",
                                "librocjpeg.so.[0-9]($|\\.[0-9\\.]+)")
 
-ROCP_REG_DEFINE_LIBRARY_TRAITS(ROCP_REG_HIPFILE,
-                               "hipFile",
-                               "rocprofiler_register_import_hipFile",
-                               "libhipfile.so.[0-9]($|\\.[0-9\\.]+)")
-
 ROCP_REG_DEFINE_LIBRARY_TRAITS(ROCP_REG_ROCATTACH,
                                "rocattach",
                                "rocprofiler_register_import_attach",
                                "librocprofiler-sdk-attach.so.[0-9]($|\\.[0-9\\.]+)")
+
+ROCP_REG_DEFINE_LIBRARY_TRAITS(ROCP_REG_HIPFILE,
+                               "hipFile",
+                               "rocprofiler_register_import_hipFile",
+                               "libhipfile.so.[0-9]($|\\.[0-9\\.]+)")
 
 ROCP_REG_DEFINE_ERROR_MESSAGE(ROCP_REG_SUCCESS, "Success")
 ROCP_REG_DEFINE_ERROR_MESSAGE(ROCP_REG_NO_TOOLS, "rocprofiler-register found no tools")
