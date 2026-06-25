db9309aad040789b581713066fd2ef69a7b1d867
James Siddeley <James.Siddeley@amd.com>
2026-06-25 22:15:07 +0000

    remvoe the analyze-side csv-format path, update tests

--- projects/rocprofiler-compute/src/utils/utils_common.py
+++ projects/rocprofiler-compute/src/utils/utils_common.py
@@ -36,6 +36,7 @@ from vendored import yaml
 # Global constants
 METRIC_ID_RE = re.compile(pattern=r"^\d{1,2}(?:\.\d{1,2}){0,2}$")
 PC_SAMPLING_BLOCK_IDS = ("21", "pc_sampling")
+_PROFILE_OUTPUT_FORMAT = "rocpd"
 
 
 def canonical_config_arch(gpu_arch: Optional[str]) -> Optional[str]:
